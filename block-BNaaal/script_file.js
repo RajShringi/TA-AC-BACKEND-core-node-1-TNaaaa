6b3aa2d1284094da54d2ab573e224c5b49666332
raj shringi <raj24shringi@gmail.com>
2022-05-25 23:45:34 +0530

    complete all todos

---
+++ block-BNaaal/script_file.js
@@ -0,0 +1,8 @@
+let http = require('http');
+let server = http.createServer(handleRequest);
+function handleRequest(req, res) {
+    res.end('Welcome')
+}
+server.listen(4000, () => {
+    console.log('Server listening on port 4000');
+})
