9630e1e30a009e08e4d12ea019c2c2b1b074f7bd
raj shringi <raj24shringi@gmail.com>
2022-05-26 16:35:42 +0530

    complete all todos

--- block-BNaaap/server.js
+++ block-BNaaap/server.js
@@ -3,6 +3,7 @@ let http = require("http");
 let server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
+  console.log(req.method);
   res.writeHead(201, { "Content-Type": "text/html" });
   res.write("<h1>HELLO</h1>");
   res.end("Welcome");
